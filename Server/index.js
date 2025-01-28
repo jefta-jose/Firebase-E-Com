8501a5a18513270f0feca2fc757fb2dd637bcedf
Jeff Ndegwa <jeff.ndegwa@griffinglobaltech.com>
2025-01-28 19:26:32 +0300

    stripe

--- Server/index.js
+++ Server/index.js
@@ -211,7 +211,50 @@ app.post("/verify-token", (req, res) => {
     res.send("Token verified!");
 });
 
+//------------------------------- STRIPE -------------->>>>
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+app.post("/checkout", async (req, res) => {
+    
+    try {
+      const { items, email } = await req.body;
+  
+      const extractingItems = await items.map((item) => ({
+        quantity: item.quantity,
+        price_data: {
+          currency: "usd",
+          unit_amount: item.discountedPrice * 100,
+          product_data: {
+            name: item.name,
+            description: item.description,
+            images: item.images,
+          },
+        },
+      }));
+  
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: extractingItems,
+        mode: "payment",
+        success_url:
+          "http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: "http://localhost:5173/cancel",
+        metadata: {
+          email,
+        },
+      });
+  
+      res.json({
+        message: "Server is connected",
+        success: true,
+        id: session.id,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  });
+  
+//------------------------------- STRIPE -------------->>>>
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
